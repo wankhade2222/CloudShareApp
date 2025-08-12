deacc24c8cf55c1266334708adc790e07d2d11e9
aryaW <aryawankhade25@gmail.com>
2025-08-13 02:04:49 +0530

    commit

--- src/components/landing/CTASection.jsx
+++ src/components/landing/CTASection.jsx
@@ -1,7 +1,19 @@
 function CTASection() {
-    return ( <div>
-        CTASection
-    </div> );
+  return <div className="bg-purple-500">
+    <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        <h2 className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
+            <span className="block">Ready to get started?</span>
+            <span className="block text-purple-100 "> Create your account today. </span>
+        </h2>
+        <div className=" mt-8 flex lg:mt-0 lg-flex-shrinks-0">
+            <div className="inline-flex rounded-md shadow" >
+                <button className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-purple-600 bg-white hover:bg-purple-50 transition-colors duartion-200 ">
+                    sign up for free
+                </button>
+            </div>
+        </div>
+    </div>
+  </div>;
 }
 
 export default CTASection;
--- src/components/landing/FooterSection.jsx
+++ src/components/landing/FooterSection.jsx
@@ -1,7 +1,15 @@
 function FooterSection() {
-    return ( <div>
-        FooterSection
-    </div> );
+    return ( 
+        <footer className="bg-gray-800">
+            <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+                <div className="text-center">
+                    <p className="text-base text-gray-400">
+                        &copy; 2025 CloudShare. All right reserved.
+                    </p>
+                </div>
+            </div>
+        </footer>
+     );
 }
 
 export default FooterSection;
